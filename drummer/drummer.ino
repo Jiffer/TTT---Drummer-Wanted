5c278f7d71602f98a6ff13eaeccb75ff0b09b05a
Jiffer <jifferh@gmail.com>
2012-10-29 11:38:04 -0600

    moved folder, debug tweaks

--- drummer/drummer.ino
+++ drummer/drummer.ino
@@ -1,7 +1,7 @@
 #include <StopWatch.h>
 
 // leaky integrator variables 
- float alpha = .05;
+ float alpha = .15;
  float a0_in = 0.0;
  float a0_last = 0.0;
  float a_out = 0.0;
@@ -9,7 +9,7 @@
  // thresholding variables
  float a_diff = 0.0;
  float W = 0.0;
- float THRESHOLD = 1.5;
+ float THRESHOLD = 2.5;
 
 // timer -- http://www.arduino.cc/playground/Code/StopWatchClass
 StopWatch timer;
@@ -42,7 +42,7 @@ void loop() {
   // registered a beat
   if(W > THRESHOLD)
   {
-    Serial.println(512);
+    //Serial.println(512);
     
     // don't register the time if it is too large (5 seconds)
     if (timer.elapsed() < 5000) 
@@ -57,32 +57,43 @@ void loop() {
       avgTime += beatTimes[i];
     }
     avgTime /= numBeats;
+    //Serial.print('avg: ');
+    
     
     if (avgTime < 100)
     {
       avgTime = 100;
     }
     
+    Serial.println(avgTime);
     avgTime -= 100;  
-    avgTime /= 4900;  // should be between 0 and 1 now
-    avgTime = 1 - avgTime;  // smaller numbers mean faster beats so inverting the order
+    avgTime /= 14900;  // should be between 0 and 1 now
+    avgTime = 1.0 - avgTime;  // smaller numbers mean faster beats so inverting the order
     
     float rate = 255 * avgTime;
+    
+    Serial.println(rate);
     analogWrite(motorPin, rate);
     
     }
     else // its been 5 seconds without a beat, make sure we're off
     { 
+      
       analogWrite(motorPin, 0);
     }
     
     // reset the timer
     timer.reset();
+    timer.start();
     
   }
   else // no new beat
   {
-    Serial.println(0);
+    if (timer.elapsed() > 5000) 
+    {
+      analogWrite(motorPin, 0);
+    }
+    //Serial.println(0);
   }
   
 }
